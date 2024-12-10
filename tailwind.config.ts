d6e823cc56a44da68873aad9752a22b0b8532644
fitronansori <fitronansori7@gmail.com>
2024-12-10 13:21:09 +0800

    Add container configuration to Tailwind CSS for improved layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,13 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        "2xl": "1440px",
+      },
+    },
     extend: {
       fontFamily: {
         inter: ["var(--font-inter)", ...fontFamily.sans],
