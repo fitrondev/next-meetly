c2f9ebd0051ca21649fd9a78674b54887f1d766e
fitronansori <fitronansori7@gmail.com>
2024-12-12 11:14:47 +0800

    Refactor RootPage component to simplify user sign-in handling

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,24 +1,18 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 
-export default async function Home() {
+const RootPage = async () => {
   const user = await currentUser();
 
   if (!user) {
-    return <p>You are not signed in.</p>;
+    return;
   }
-
   return (
     <div>
-      <Button>Click me</Button>
-
-      <UserButton />
-
       <p>
         Hello, {user.fullName}! Your email is{" "}
         {user.primaryEmailAddress?.emailAddress}
       </p>
     </div>
   );
-}
+};
+export default RootPage;
