67e1d430ca76330784531ae0ed320eca5f77b16e
fitronansori <fitronansori7@gmail.com>
2024-12-12 11:13:38 +0800

    Update Logo component to support dark and light themes

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,16 +1,41 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
+import { useTheme } from "next-themes";
+
 const Logo = () => {
+  const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
   return (
     <Link href={"/"}>
-      <Image
-        src={"/assets/icons/logo.svg"}
-        alt="logoipsum"
-        width={100}
-        height={35}
-        className="w-16 h-9"
-      />
+      {theme === "dark" ? (
+        <Image
+          src={"/assets/icons/logo-white.svg"}
+          alt="logoipsum-white"
+          width={100}
+          height={35}
+          className="h-9 w-16"
+        />
+      ) : (
+        <Image
+          src={"/assets/icons/logo.svg"}
+          alt="logoipsum-dark"
+          width={100}
+          height={35}
+          className="h-9 w-16"
+        />
+      )}
     </Link>
   );
 };
